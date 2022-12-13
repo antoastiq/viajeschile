f8de5db5fcce9ff4b3645605338664443e6508c7
antoastiq <m.antoniet20@gmail.com>
2022-12-13 20:26:12 -0300

    alert included - sección contacto

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,13 @@
-//Tooltips
+//Tooltips - Footer
 
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+
+//Alert - Sección Contacto
+
+var texto = "¡Gracias por tu mensaje! Pronto te contactaremos."
+$("#sendmessage").click(function(){
+    alert(texto)
+})
